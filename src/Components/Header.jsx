2249b3aa3e900957f7d58dd95da73313983d07a8
Ayan Upadhaya <ayanu881@gmail.com>
2023-09-05 11:43:21 +0600

    added navbar

---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+    const cart = [];
+    return (
+        <div>
+            <nav className={'navbar navbar-expand-lg bg-dark fixed-top w-100 text-white":"navbar navbar-expand-lg bg-dark w-100 text-white'}>
+                <div className="container">
+                    <a className="navbar-brand text-white fw-bold" href="#">FoodExpress</a>
+                    <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form className="ms-auto">
+                            <a className="btn btn-lg btn-warning fw-bold rounded" to="/cart"><i class="bi bi-cart"></i><span>{cart.length}</span></a>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
